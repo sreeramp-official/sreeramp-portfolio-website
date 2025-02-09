9ba299002b3c40d49e98e519796f713ef83208cc
Sreeram P <114813900+sreeramp-official@users.noreply.github.com>
2025-02-09 22:15:44 +0530

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -20,7 +20,7 @@ const CONFIG = {
         limit: 10, // How many projects to display.
         exclude: {
           forks: false, // Forked projects will not be displayed if set to true.
-          projects: ['sreeramp-official/sreeramp-portfolio-website'], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+          projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
       manual: {
